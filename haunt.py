f783c3525bf903aa8a0ed46358b82f93b559db06
Justin Clark Ong <justinclarkong@gmail.com>
2024-06-16 09:49:33 +0800

    haunt.py: remove magic numbers for indexing GPS IFD

--- haunt.py
+++ haunt.py
@@ -11,12 +11,13 @@ img = Image.open(fname)
 
 exif = img.getexif()
 gps = exif.get_ifd(ExifTags.IFD.GPSInfo)
-gps[1] = random.choice(["N", "S"])
-gps[2] = (scramble(0, 90, 1), scramble(0, 60), scramble(0, 60))
-gps[3] = random.choice(["W", "E"])
-gps[4] = (scramble(0, 180, 1), scramble(0, 60), scramble(0, 60))
+gps[ExifTags.GPS.GPSLatitudeRef] = random.choice(["N", "S"])
+gps[ExifTags.GPS.GPSLatitude] = (scramble(0, 90, 1), scramble(0, 60), scramble(0, 60))
+gps[ExifTags.GPS.GPSLongitudeRef] = random.choice(["W", "E"])
+gps[ExifTags.GPS.GPSLongitude] = (scramble(0, 180, 1), scramble(0, 60), scramble(0, 60))
 print(gps)
 print(exif)
+print(ExifTags.Base.Make.value)
 
 for k, v in exif.items():
     print(f"Tag: {ExifTags.TAGS[k]}, Value: {v}")
